c0514ff0d7d44f350450b0b3c2a8d6f85c60556b
Gian Machella <gianmachella@Gians-MBP.attlocal.net>
2025-09-29 10:47:24 -0500

    fix connect DB

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -84,7 +84,7 @@ export default function Hero() {
         </p>
       `,
         confirmButtonText: "Got it!",
-        width: 500,
+        width: 510,
         padding: "1.5rem",
         showCloseButton: true,
       });
