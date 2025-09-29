881b4810496206df16dadeb0a7548d455e651083
Gian Machella <gianmachella@Gians-MBP.attlocal.net>
2025-09-29 11:08:00 -0500

    fix connect DB

--- prisma/migrations/20250916171835_normalize_units/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - The values [G,KG,MG,ML,L,OZ,LB,FLOZ,CUP,TBSP,TSP,PT,QT,GAL,UNIT,PACK] on the enum `Ingredient_unitType` will be removed. If these variants are still used in the database, this will fail.
-
-*/
--- AlterTable
-ALTER TABLE `Ingredient` MODIFY `unitType` ENUM('g', 'kg', 'mg', 'ml', 'l', 'oz', 'lb', 'floz', 'cup', 'tbsp', 'tsp', 'pt', 'qt', 'gal', 'unidad', 'pack') NOT NULL;
